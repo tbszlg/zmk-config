0d1ed9a877c7bb56c1e7d07f96f3c6f4c8ce40dd
Tobias Zillig <tobizillig@gmail.com>
2025-01-04 16:35:41 +0100

    remove unwanted sections from main screen

--- boards/shields/nice_view_cstm/widgets/screen.c
+++ boards/shields/nice_view_cstm/widgets/screen.c
@@ -209,21 +209,11 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    // lv_obj_t *middle = lv_canvas_create(widget->obj);
-    // lv_obj_align(middle, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_MIDDLE, 0);
-    // lv_canvas_set_buffer(middle, widget->cbuf2, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
-
-    // lv_obj_t *bottom = lv_canvas_create(widget->obj);
-    // lv_obj_align(bottom, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_BOTTOM, 0);
-    // lv_canvas_set_buffer(bottom, widget->cbuf3, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
-
     draw_animation(widget->obj);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
-    // widget_layer_status_init();
     widget_output_status_init();
-    // widget_wpm_status_init();
 
     return 0;
 }
